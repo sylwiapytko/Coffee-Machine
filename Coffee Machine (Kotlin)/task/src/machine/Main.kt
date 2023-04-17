2aa94e72e1228236a60f22148333ef6ddba6da9a
sylwiaptko <sylwia.pytko@gmail.com>
2023-04-17 11:58:20 +0200

    Task stage 5 completed

--- Coffee Machine (Kotlin)/task/src/machine/Main.kt
+++ Coffee Machine (Kotlin)/task/src/machine/Main.kt
@@ -1,11 +1,7 @@
 package machine
 
-const val WATER_PER_CUP = 200
-const val MILK_PER_CUP = 50
-const val BEANS_PER_CUP = 15
-
 class CoffeeSpecs(val water: Int, val milk: Int, val beans: Int, val cost: Int) {}
-class CoffeeMachineSpecs(var water: Int = 400, var milk: Int = 540, var beans: Int= 120, var cups: Int = 9 , var cash: Int = 550)
+class CoffeeMachineSpecs(var water: Int = 400, var milk: Int = 540, var beans: Int = 120, var cups: Int = 9, var cash: Int = 550)
 
 fun main() {
     val espresso = CoffeeSpecs(250, 0, 16, 4)
@@ -15,16 +11,16 @@ fun main() {
     val coffeeList = listOf<CoffeeSpecs>(espresso, latte, cappuccino)
     val coffeeMachineSpecs = CoffeeMachineSpecs()
 
-    printCoffeeMachineSpecs(coffeeMachineSpecs)
-
-    println("Write action (buy, fill, take):")
-    val action = readln()
-    when (action) {
-        "buy" -> buyCoffee(coffeeMachineSpecs, coffeeList)
-        "fill" -> fillCoffeeMachine(coffeeMachineSpecs)
-        "take" -> takeCoffeeMoney(coffeeMachineSpecs)
-    }
-    printCoffeeMachineSpecs(coffeeMachineSpecs)
+    do {
+        println("Write action (buy, fill, take, remaining, exit):")
+        val action = readln()
+        when (action) {
+            "buy" -> buyCoffee(coffeeMachineSpecs, coffeeList)
+            "fill" -> fillCoffeeMachine(coffeeMachineSpecs)
+            "take" -> takeCoffeeMoney(coffeeMachineSpecs)
+            "remaining" -> printCoffeeMachineSpecs(coffeeMachineSpecs)
+        }
+    } while (action != "exit")
 
 }
 
@@ -47,51 +43,60 @@ private fun printCoffeeMakingActions() {
     println("Coffee is ready!")
 }
 
-private fun checkCoffeeAvailability(waterAvailable: Int, milkAvailable: Int, beansAvailable: Int) {
-    val cupsRequested = readln().toInt()
-    val maxCups = calculateMaxCups(waterAvailable, milkAvailable, beansAvailable)
-
-    if (cupsRequested > maxCups) {
-        println("No, I can make only $maxCups cups of coffee")
-    } else if (cupsRequested == maxCups) {
-        println("Yes, I can make that amount of coffee")
-    } else println("Yes, I can make that amount of coffee (and even ${maxCups - cupsRequested} more than that)")
-}
-
 fun takeCoffeeMoney(coffeeMachineSpecs: CoffeeMachineSpecs) {
     println("I gave you ${coffeeMachineSpecs.cash}")
-    coffeeMachineSpecs.cash=0
+    coffeeMachineSpecs.cash = 0
 }
 
 fun buyCoffee(coffeeMachineSpecs: CoffeeMachineSpecs, coffeeList: List<CoffeeSpecs>) {
     println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-    when(readln()){
-        "1"-> makeCoffee(coffeeMachineSpecs, coffeeList[0])
-        "2"-> makeCoffee(coffeeMachineSpecs, coffeeList[1])
-        "3"-> makeCoffee(coffeeMachineSpecs, coffeeList[2])
+    when (readln()) {
+        "1" -> makeCoffee(coffeeMachineSpecs, coffeeList[0])
+        "2" -> makeCoffee(coffeeMachineSpecs, coffeeList[1])
+        "3" -> makeCoffee(coffeeMachineSpecs, coffeeList[2])
     }
 }
 
 fun makeCoffee(coffeeMachineSpecs: CoffeeMachineSpecs, coffeeSpecs: CoffeeSpecs) {
+
+    val enoughWater = enoughResources(coffeeMachineSpecs.water, coffeeSpecs.water)
+    val enoughMilk = enoughResources(coffeeMachineSpecs.milk, coffeeSpecs.milk)
+    val enoughBeans = enoughResources(coffeeMachineSpecs.beans, coffeeSpecs.beans)
+    val enoughCups = coffeeMachineSpecs.cups>=1
+
+    if (enoughWater && enoughMilk && enoughBeans && enoughCups) {
+        println("I have enough resources, making you a coffee!")
+        useResources(coffeeMachineSpecs, coffeeSpecs)
+    } else {
+        print("Sorry, not enough ")
+        when {
+            !enoughWater -> println("water!")
+            !enoughMilk -> println("milk!")
+            !enoughBeans -> println("beans!")
+            !enoughCups -> println("cups!")
+        }
+    }
+}
+
+fun useResources(coffeeMachineSpecs: CoffeeMachineSpecs, coffeeSpecs: CoffeeSpecs) {
     coffeeMachineSpecs.water -= coffeeSpecs.water
     coffeeMachineSpecs.milk -= coffeeSpecs.milk
     coffeeMachineSpecs.beans -= coffeeSpecs.beans
-    coffeeMachineSpecs.cups --
+    coffeeMachineSpecs.cups--
     coffeeMachineSpecs.cash += coffeeSpecs.cost
+}
+
+fun enoughResources(resourceAvailable: Int, resourceNeeded: Int): Boolean {
+    return if(resourceNeeded==0) true else resourceAvailable / resourceNeeded >= 1
 }
 
 fun fillCoffeeMachine(coffeeMachineSpecs: CoffeeMachineSpecs) {
     println("Write how many ml of water you want to add:")
-     coffeeMachineSpecs.water += readln().toInt()
+    coffeeMachineSpecs.water += readln().toInt()
     println("Write how many ml of milk you want to add:")
     coffeeMachineSpecs.milk += readln().toInt()
     println("Write how many grams of coffee beans you want to add:")
     coffeeMachineSpecs.beans += readln().toInt()
     println("Write how many disposable cups you want to add:")
     coffeeMachineSpecs.cups += readln().toInt()
-}
-
-fun calculateMaxCups(waterAvailable: Int, milkAvailable: Int, beansAvailable: Int): Int {
-
-    return minOf(waterAvailable / WATER_PER_CUP, milkAvailable / MILK_PER_CUP, beansAvailable / BEANS_PER_CUP)
 }
